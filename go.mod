be903ae8a7905e1cb021c2552992d7445d604548
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-08-14 18:28:38 +0800

    initial module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Belajar-Bareng/simple-hello-go
+
+go 1.17
---
+++ say_hello.go
@@ -0,0 +1,52 @@
+package simplehellogo
+
+import "fmt"
+
+var lang string
+var langs map[string]string = map[string]string{
+	"ID": "Halo! Salam kenal",
+	"EN": "Hello there",
+}
+
+func init() {
+	lang = "ID"
+}
+
+func Hello() string {
+	return langs[lang]
+}
+
+func SayHello() {
+	fmt.Println(Hello())
+}
+
+func GreetPerson(name string) string {
+	return Hello() + ", " + name
+}
+
+func SayGreetPerson(name string) {
+	fmt.Println(GreetPerson(name))
+}
+
+func GetCurrentLanguage() string {
+	return lang
+}
+
+func GetAvailableLanguages() []string {
+	var avl []string
+	for cd := range langs {
+		avl = append(avl, cd)
+	}
+	return avl
+}
+
+func SetLanguange(language string) bool {
+	for cd := range langs {
+		if language == cd {
+			lang = language
+			return true
+		}
+	}
+
+	return false
+}
